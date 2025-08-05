8618109acbed3f7951fc1b97506be890429d26f4
Felipe Messias <felipe.messias.fms@gmail.com>
2025-08-05 17:32:16 -0300

    feat: add category selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategorySelector from "@/components/common/category-selector";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
@@ -7,6 +8,8 @@ export default async function Home() {
   const products = await db.query.productTable.findMany({
     with: { variants: true },
   });
+
+  const categories = await db.query.categoryTable.findMany({});
   return (
     <>
       <Header />
@@ -23,6 +26,10 @@ export default async function Home() {
         </div>
 
         <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
 
         <div className="px-5">
           <Image
